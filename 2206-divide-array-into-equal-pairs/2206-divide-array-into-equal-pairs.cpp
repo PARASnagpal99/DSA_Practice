fcab9ea67a3c6de5b22d7c7a370ddc301ea15f7a
Paras Nagpal <paras.nagpal743@gmail.com>
2022-04-19 22:47:23 +0530

    Time: 19 ms (61.26%), Space: 13.1 MB (85.14%) - LeetHub

---
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        int i = 0 ;
+        while(i<n){
+            if(nums[i]!=nums[i+1]){
+                return false ;
+            }
+            i = i+2 ;
+        }
+        return true ;
+    }
+};
