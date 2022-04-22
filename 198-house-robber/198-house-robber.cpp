a04e9e32104516a22eecb1fb4893f19c0987a927
Paras Nagpal <paras.nagpal743@gmail.com>
2022-04-23 02:05:59 +0530

    Time: 8 ms (5.53%), Space: 7.7 MB (76.31%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    int solve(int indx , vector<int>& arr , vector<int>& dp ){
-        if(indx==0) return arr[indx] ;
-        if(indx<0) return 0 ;
-        if(dp[indx]!=-1) return dp[indx];
+//     int solve(int indx , vector<int>& arr , vector<int>& dp ){
+//         if(indx==0) return arr[indx] ;
+//         if(indx<0) return 0 ;
+//         if(dp[indx]!=-1) return dp[indx];
         
-        int pick = arr[indx] + solve(indx-2,arr,dp);
-        int notPick = solve(indx-1,arr,dp) ;
+//         int pick = arr[indx] + solve(indx-2,arr,dp);
+//         int notPick = solve(indx-1,arr,dp) ;
         
-        return dp[indx] = max(pick,notPick) ;
+//         return dp[indx] = max(pick,notPick) ;
         
-    }
+//     }
     
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1,-1);
-        int ans = solve(n-1,nums,dp);
-        return ans ;
+        int prev = nums[0] ;
+        int prev2 = 0 ;
+        for(int i=1 ; i<n ; ++i){
+            int pick = nums[i] ;
+            if(i>1){
+                pick+=prev2;
+            }
+            int nonPick = prev ;
+            int curr = max(pick,nonPick);
+            prev2 = prev ;
+            prev = curr ;
+        }
+        return prev ;
     }
 };
