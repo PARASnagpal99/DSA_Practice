51759cf6a38a310ce63e0c0a933b8bf21107b4f4
Paras Nagpal <paras.nagpal743@gmail.com>
2022-04-22 21:11:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (46.03%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double ans = 1.00000 ;
-        long long nn = n ;
-        if(nn<0) nn = (-1)*nn ;
-        while(nn){
-            if(nn%2){
-                ans = ans * x ;
-                nn = nn -1 ;
-            }else{
-                x*=x ;
-                nn=nn/2 ;
-            }
+        if(n==0) return 1 ;
+         if(n<0){
+            n = abs(n);
+            x = 1/x ;
         }
-     if(n<0){
-         ans = (double)(1.0)/(double)(ans) ;    
-     }
-     return ans ;
+        if(n%2==0){
+            double y = myPow(x,n/2);
+            return (y*y);
+        }else{
+            return (x*myPow(x,n-1));
+        }
     }
 };
