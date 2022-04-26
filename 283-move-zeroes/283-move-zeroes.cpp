48ab01a24f5024268233a68fdbac1078961a19f2
Paras Nagpal <paras.nagpal743@gmail.com>
2022-04-26 12:16:13 +0530

    Time: 26 ms (71.70%), Space: 19.3 MB (14.14%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+          int n = nums.size() ;
+          int i = 0 ;
+          int prev = 0 ;
+          while(i<n){
+              if(nums[i]!=0){
+                  swap(nums[i],nums[prev]);
+                  prev++;
+              }
+              i++;
+          }
+    }
+};
