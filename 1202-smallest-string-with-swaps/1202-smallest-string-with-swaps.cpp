5aa48bfdff101f8d1b8131699145e9a7c0fb8b9e
Paras Nagpal <paras.nagpal743@gmail.com>
2022-04-27 21:00:12 +0530

    Time: 391 ms (15.90%), Space: 67.8 MB (25.37%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -1,62 +1,43 @@
 class Solution {
 public:
-static const int N = 1e5 + 7 ;
-    int parent[N] ;
-    int size[N] ;
-    
-    void make(int v){
-        parent[v] = v ;
-        size[v] = 1 ;
-    }
-    
-    int find(int v){
-        if(v==parent[v]) return v ;
-        return parent[v] = find(parent[v]) ;
-    }
-    
-    void Union(int a , int b){
-        a = find(a) ;
-        b = find(b) ;
-        if(a!=b){
-            if(size[a]<size[b]){
-                swap(a,b);
-            }
-            parent[b] = a ;
-            size[a]+=size[b] ;
+    static const int N = 1e5 + 7 ;
+    vector<int> g[N] ;
+    bool visited[N] ;
+    void dfs(string &s , int vertex , vector<char>& characters , vector<int>& indices){
+        characters.push_back(s[vertex]);
+        indices.push_back(vertex);
+        visited[vertex] = true ;
+        
+        for(auto child : g[vertex]){
+             if(!visited[child]){
+                 dfs(s,child,characters,indices);
+             }
         }
     }
+    
+    
     
     string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
-        int n = s.length() ;
-        
-        if(pairs.size()==0) return s ;
-        
-        for(int i=1 ; i<=n ; ++i){
-              make(i) ;
-        }
-    
         for(auto &it : pairs){
-            Union(it[0],it[1]);
+            int x = it[0] ;
+            int y = it[1] ;
+            g[x].push_back(y);
+            g[y].push_back(x);
         }
         
-        map<int,vector<int>>mp ;
-        for(int i=0 ; i<n ; ++i){
-            mp[find(i)].push_back(i) ;
+        for(int vertex=0 ; vertex<s.length() ; ++vertex){
+            if(!visited[vertex]){
+                vector<char> characters ;
+                vector<int> indices ;
+                dfs(s,vertex,characters,indices);
+                sort(characters.begin(),characters.end());
+                sort(indices.begin(),indices.end());
+                for(int i=0 ; i<characters.size() ; ++i){
+                    s[indices[i]] = characters[i] ;
+                }
+            }
         }
         
-        for(auto &it : mp){
-            string st = "" ;
-            for(auto &t : it.second){
-                st+=s[t] ;
-            }
-            
-            sort(st.begin(),st.end());
-            for(int i=0 ; i<it.second.size() ; ++i){
-                s[it.second[i]] = st[i] ;
-            }
-        }
         return s ;
-        
-        
     }
 };
