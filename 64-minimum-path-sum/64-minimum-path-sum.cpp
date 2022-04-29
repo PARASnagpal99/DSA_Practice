77b2ed69fff56971f52f7e42f0f120b8399d30d2
Paras Nagpal <paras.nagpal743@gmail.com>
2022-04-30 00:53:57 +0530

    Time: 13 ms (47.82%), Space: 10.3 MB (6.58%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -10,8 +10,25 @@ public:
         return dp[i][j] = min(up,left);
     }
     int minPathSum(vector<vector<int>>& grid) {
-          int m = grid.size() , n = grid[0].size();
-          memset(dp,-1,sizeof(dp));
-          return func(m-1,n-1,grid);
+    int n = grid.size() , m = grid[0].size() ;
+    vector<vector<int> > dp(n,vector<int>(m,0));
+    for(int i=0; i<n ; i++){
+        for(int j=0; j<m; j++){
+            if(i==0 && j==0) dp[i][j] = grid[i][j];
+            else{
+                
+                int up = grid[i][j];
+                if(i>0) up += dp[i-1][j];
+                else up += 1e9;
+                
+                int left = grid[i][j];
+                if(j>0) left+=dp[i][j-1];
+                else left += 1e9;
+                
+                dp[i][j] = min(up,left);
+            }
+        }
+     }
+       return dp[n-1][m-1];
     }
 };
