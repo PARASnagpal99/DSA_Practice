c1afa881462265266ee2166659ba23070824ac2b
Paras Nagpal <paras.nagpal743@gmail.com>
2022-04-29 23:48:12 +0530

    Time: 68 ms (70.40%), Space: 41.9 MB (57.68%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    if(num<1) return false 
+    if(num==1)return true 
+    let left = 1 
+    let right = num 
+    while(left<=right){
+       let mid = Math.floor(left+(right-left)/2)
+       if(mid*mid==num) return true 
+       else if(mid*mid>num)
+           right = mid-1
+       else
+           left = mid+1
+    }
+    return false
+};
