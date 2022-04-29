4fe23d73b86fbad8d7f39597fcf7ada5e6b8b3be
Paras Nagpal <paras.nagpal743@gmail.com>
2022-04-29 21:53:51 +0530

    Time: 25 ms (78.29%), Space: 13.5 MB (81.82%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool bfs(vector<vector<int>>& graph ,vector<int>&visited, int s){
-        int n = graph.size();
         queue<int>q ;
         visited[s] = 1 ;
         q.push(s);
@@ -21,12 +20,24 @@ public:
         return true ;
     }
     
+    bool dfs(vector<vector<int>>& graph ,vector<int>&visited, int s , int colr){
+        visited[s] = colr ;
+        for(auto child : graph[s]){
+            if(!visited[child]){
+                if(dfs(graph ,visited, child , -1*visited[s])==false) return false;
+            }else if(visited[child]==visited[s]){
+                return false ;
+            }
+        }
+        return true ;
+    }
+    
     bool isBipartite(vector<vector<int>>& graph) {
            int n = graph.size();
            vector<int>visited(n,0);
              for(int i=0 ; i<n ; ++i){
                  if(!visited[i]){
-                     if(bfs(graph,visited,i)==false) return false;
+                     if(dfs(graph,visited,i,1)==false) return false;
                  }
              }
         return true ;
