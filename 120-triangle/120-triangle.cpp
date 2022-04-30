875c3acb4ed0793a24ef32d7d65ee6c2aba5e068
Paras Nagpal <paras.nagpal743@gmail.com>
2022-04-30 17:43:51 +0530

    Time: 3 ms (98.09%), Space: 8.7 MB (50.86%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -10,7 +10,21 @@ public:
         return dp[i][j] = min(ans1,ans2);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
-        memset(dp,-1,sizeof(dp));
-        return func(0,0,triangle);
+        int n = triangle.size() ;
+        vector<int>front(n,0),cur(n,0) ;
+        for(int i=0 ; i<n ; ++i){
+            front[i] = triangle[n-1][i] ;
+        }
+        for(int i=n-2; i>=0; i--){
+        for(int j=i; j>=0; j--){
+            
+            int down = triangle[i][j]+front[j];
+            int diagonal = triangle[i][j]+front[j+1];
+            
+            cur[j] = min(down, diagonal);
+        }
+        front=cur;
+      }
+    return front[0] ;
     }
 };
