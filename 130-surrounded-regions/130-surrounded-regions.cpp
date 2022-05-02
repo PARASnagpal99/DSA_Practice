b182b4b934d49918b1ad5cea9980910dab951d6d
Paras Nagpal <paras.nagpal743@gmail.com>
2022-05-02 14:08:39 +0530

    Time: 19 ms (43.75%), Space: 9.9 MB (91.73%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid , int i , int j){
+        int m = grid.size() , n = grid[0].size() ;
+        if(i<0 or j<0 or i>=m or j>=n) return ;
+        if(grid[i][j]!='O') return ;
+        grid[i][j] = '@' ;
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+    }
+
+    void solve(vector<vector<char>>& grid) {
+          int m = grid.size() , n = grid[0].size() ;
+          for(int i=0 ; i<m ; ++i){
+              if(grid[i][0]=='O'){
+                  dfs(grid,i,0);
+              }
+              if(grid[i][n-1]=='O'){
+                  dfs(grid,i,n-1);
+              }
+          }
+        for(int i=0 ; i<n ; ++i){
+              if(grid[0][i]=='O'){
+                  dfs(grid,0,i);
+              }
+              if(grid[m-1][i]=='O'){
+                  dfs(grid,m-1,i);
+              }
+          }
+        
+        for(int i=0 ; i<m ; ++i){
+            for(int j=0 ; j<n ; ++j){
+                if(grid[i][j]=='O'){
+                    grid[i][j]='X';
+                }
+                if(grid[i][j]=='@'){
+                    grid[i][j]='O';
+                }
+            }
+        }
+    }
+};
