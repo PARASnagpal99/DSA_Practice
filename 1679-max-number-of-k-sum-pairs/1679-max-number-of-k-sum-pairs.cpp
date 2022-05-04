ceb0d21d73235365e0f93f12deecbbb4e061900a
Paras Nagpal <paras.nagpal743@gmail.com>
2022-05-04 10:40:32 +0530

    Time: 301 ms (13.84%), Space: 68.2 MB (32.58%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -3,14 +3,18 @@ public:
     int maxOperations(vector<int>& nums, int k) {
         int n = nums.size() ;
         map<int,int>mp ;
+        for(auto &it : nums){
+            mp[it]++;
+        }
         int count = 0 ;
-        for(int i=0 ; i<n ; ++i){
-            int x = nums[i] ;
-            if(mp[k-x]>0){
-                count++;
-                mp[k-x]--;
-            }else{
-                mp[x]++;
+        for(auto &it : mp){
+            int num = it.first , freq = it.second ;
+            if(num==k-num) count+=(freq/2);
+            else if(mp.find(k-num)!=mp.end()){
+                int x = min(mp[num] , mp[k-num]);
+                count+=x ;
+                mp[num]-=x;
+                mp[k-num]-=x;
             }
         }
         return count ;
