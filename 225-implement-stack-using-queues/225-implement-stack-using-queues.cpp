1b9ac9032334f477ca7db49a07535c2086a7611c
Paras Nagpal <paras.nagpal743@gmail.com>
2022-05-05 23:15:00 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (88.02%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,7 +1,7 @@
 class MyStack {
 private : 
     queue<int>q1 ;
-    queue<int>q2 ;
+  //  queue<int>q2 ;
 public:
     MyStack() {
         // queue<int>q1 ;
@@ -10,38 +10,48 @@ public:
     
     void push(int x) {
         q1.push(x);
+        for(int i=0 ; i<q1.size()-1 ; ++i){
+            int x = q1.front() ;
+            q1.pop();
+            q1.push(x);
+        }
     }
     
     int pop() {
-        int x = q1.size() ;
-        for(int i=0 ; i<x-1 ; ++i){
-            int val = q1.front() ;
-            q1.pop();
-            q2.push(val);
-        }
-        int ans = q1.front() ;
+        // int x = q1.size() ;
+        // for(int i=0 ; i<x-1 ; ++i){
+        //     int val = q1.front() ;
+        //     q1.pop();
+        //     q2.push(val);
+        // }
+        // int ans = q1.front() ;
+        // q1.pop() ;
+        // q1 = q2 ;
+        // while(!q2.empty()){
+        //     q2.pop() ;
+        // }
+        // return ans ;
+        int x = q1.front() ;
         q1.pop() ;
-        q1 = q2 ;
-        while(!q2.empty()){
-            q2.pop() ;
-        }
-        return ans ;
+        return x ;
     }
     
     int top() {
-        int x = q1.size() ;
-        for(int i=0 ; i<x-1 ; ++i){
-            int val = q1.front() ;
-            q1.pop();
-            q2.push(val);
-        }
-        int ans = q1.front() ;
-        q1 = q2 ;
-        q1.push(ans) ;
-        while(!q2.empty()){
-            q2.pop() ;
-        }
-        return ans ;
+        // int x = q1.size() ;
+        // for(int i=0 ; i<x-1 ; ++i){
+        //     int val = q1.front() ;
+        //     q1.pop();
+        //     q2.push(val);
+        // }
+        // int ans = q1.front() ;
+        // q1 = q2 ;
+        // q1.push(ans) ;
+        // while(!q2.empty()){
+        //     q2.pop() ;
+        // }
+        // return ans ;
+        int x = q1.front() ;
+        return x ;
     }
     
     bool empty() {
