46b7e049cbdb93416dd7b7c728ea73d3bbe7d3d9
Paras Nagpal <paras.nagpal743@gmail.com>
2022-05-11 21:19:17 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (34.92%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
-    void func(vector<vector<char>>&res , vector<char>&temp , int indx , vector<char> &arr , int n){
-        if(temp.size()==n){
-            res.push_back(temp);
-            return ;
-        }
-        
-        for(int i=indx ; i<arr.size() ; ++i){
-            temp.push_back(arr[i]);
-            func(res,temp,i,arr,n);
-            temp.pop_back();
-        }
-    }
-    
     int countVowelStrings(int n) {
-          vector<char> arr = {'a','e','i','o','u'};
-          vector<vector<char>>res ;
-          vector<char>temp ;
-          func(res,temp,0,arr,n);
-          return res.size() ;
+        vector<int> dp(5,1);
+        for(int i=0 ; i<n ; ++i){
+            for(int j=1 ; j<5 ; ++j){
+                dp[j]+=dp[j-1];
+            }
+        }
+        return dp[4];
     }
 };
