e609a441f492945393e59d00f86241c8c2b6f6b2
Paras Nagpal <paras.nagpal743@gmail.com>
2022-05-12 09:18:13 +0530

    Time: 3 ms (97.10%), Space: 8.5 MB (81.01%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    void func( vector<vector<int>>&ans , vector<int>&temp , map<int,int> &s , int n ){
+          if(temp.size()==n){
+              ans.push_back(temp);
+              return ;
+          } 
+        for(auto &it : s){
+            if(it.second<=0){
+                continue ;
+            }
+            it.second--;
+            temp.push_back(it.first);
+            func(ans,temp,s,n);
+            it.second++;
+            temp.pop_back();
+        }
+    }
+    
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>>ans ;
+        vector<int>temp ;
+        map<int,int>s ;
+        for(auto &it : nums){
+            s[it]++;
+        }
+        func(ans,temp,s,n);
+        return ans ;
+    }
+};
+
+
+
+
+/*
+
+
+1 1 2 
+
+
+
+
+
+
+
+
+
+
+
+*/
