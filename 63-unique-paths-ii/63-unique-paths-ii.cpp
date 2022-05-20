709ca607d50c7254ad187934ad8fd5a9dd2cc7d9
Paras Nagpal <paras.nagpal743@gmail.com>
2022-05-20 18:56:38 +0530

    Time: 7 ms (31.15%), Space: 7.7 MB (60.56%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
-    int func(int i , int j , vector<vector<int>>&grid , vector<vector<int>>&dp){
-         if(i<0 or j<0 or grid[i][j]==1) return 0 ;
-         if(i==0 && j==0) return 1 ;
-         if(dp[i][j]) return dp[i][j] ;
-         return dp[i][j] = func(i-1,j,grid,dp) + func(i,j-1,grid,dp) ;
+   int func(vector<vector<int>>& grid ,  vector<vector<int>>& dp){
+          int m = grid.size() , n = grid[0].size() ;
+          for(int i=0 ; i<m ; ++i){
+              for(int j=0 ; j<n ; ++j){
+                  if(i>=0 && j>=0 && grid[i][j]==1){
+                      dp[i][j] = 0 ;
+                      continue ;
+                  }
+                  if(i==0 && j==0){
+                      dp[i][j] = 1 ;
+                      continue ;
+                  }
+                  int left = 0 ;
+                  int up = 0 ;
+                  if(i>0)left = dp[i-1][j];
+                  
+                  if(j>0)up =dp[i][j-1] ;
+                  
+                  dp[i][j] = up + left ;
+                  
+              }
+          }    
+        return dp[m-1][n-1];
     }
     
     int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
         int m = Grid.size() , n = Grid[0].size() ;
         vector<vector<int>>dp(m,vector<int>(n,0));
-        return func(m-1,n-1,Grid,dp);
+        return func(Grid,dp);
     }
 };
