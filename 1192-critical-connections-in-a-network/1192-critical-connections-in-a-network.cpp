6648c2cd20613a631ef9827b0bf8efcf2fddeb7f
Paras Nagpal <paras.nagpal743@gmail.com>
2022-05-20 22:08:40 +0530

    Time: 711 ms (78.12%), Space: 175.2 MB (75.78%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
-   vector<vector<int>> ans;
-	vector<vector<int>> graph;
-
-	vector<int> firstTime, minTime, visited;
-	int time = 1;
-
-	void dfs(int node, int parent = -1) {
-		firstTime[node] = minTime[node] = time++;
-		visited[node] = true;
-
-		for(auto& child : graph[node]) {
-			if(child == parent)
-				continue;
-
-			if(!visited[child]) 
-				dfs(child, node);
-
-			minTime[node] = min(minTime[child], minTime[node]);
-
-			if(firstTime[node] < minTime[child])
-				ans.push_back({node, child});
-		}
-	}
-
-	vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-		firstTime.resize(n), minTime.resize(n), visited.resize(n), graph.resize(n);
-		for(auto& edge : connections) {
-			int nodeA = edge[0], nodeB = edge[1];
-			graph[nodeA].push_back(nodeB);
-			graph[nodeB].push_back(nodeA);
-		}
-		dfs(0);
-		return ans;
-	}
+    vector<vector<int>>ans ;
+    void dfs(int node , int parent , vector<int> &vis , vector<int> &tin , vector<int> &low , int &timer , vector<int> graph[]){
+            vis[node] = 1 ;
+            tin[node] = low[node] = timer++;
+            for(auto &it : graph[node]){
+                if(it==parent) continue ;
+                
+                if(!vis[it]){
+                    dfs(it,node,vis,tin,low,timer,graph);
+                    low[node] = min(low[node],low[it]);
+                    if(low[it]>tin[node]){
+                        ans.push_back({node,it});
+                    }
+                }else{
+                    low[node] = min(low[node],tin[it]);
+                }
+            }
+    }
+    
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+            vector<int> graph[n] ;
+            for(int i=0 ; i<connections.size() ; ++i){
+                     int u = connections[i][0] ;
+                     int v = connections[i][1] ;
+                graph[u].push_back(v);
+                graph[v].push_back(u) ;
+            }
+        
+        vector<int> tin(n,-1);
+        vector<int> low(n,-1);
+        vector<int> vis(n,0);
+        int timer = 0 ;
+        for(int i=0 ; i<n ; ++i){
+            if(!vis[i]){
+                dfs(i,-1,vis,tin,low,timer,graph);
+            }
+        }
+       return ans ;   
+    }
 };
