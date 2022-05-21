76e64fa61aa7fb1ce05ee6c695f8afc6d1617881
Paras Nagpal <paras.nagpal743@gmail.com>
2022-05-21 16:37:46 +0530

    Time: 64 ms (91.49%), Space: 11.6 MB (75.31%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,49 +1,23 @@
 class Solution {
 public:
-    int func(vector<int>& arr ,  vector<vector<int>>&dp , int target , int indx){
-        if(indx==0){
-            if(target%arr[indx]==0){
-                return target/arr[indx] ;
-            }else{
-                return 1e9 ;
-            }
-        }
+    int dp[10010];
+    
+    int func(int amount , vector<int> &coins){
+        if(amount==0) return 0 ;
+        if(dp[amount]!=-1) return dp[amount] ;
         
-        if(dp[indx][target]!=-1) return dp[indx][target] ;
-        
-        int notTaken = 0 + func(arr,dp,target,indx-1);
-        int taken = 1e9 ;
-        if(target>=arr[indx]){
-            taken = 1 + func(arr,dp,target-arr[indx],indx);
+        int ans = INT_MAX ;
+        for(auto coin : coins){
+            if(amount-coin>=0)
+                ans = min(ans+0LL , func(amount-coin , coins) + 1LL) ;
         }
-        return dp[indx][target] = min(taken,notTaken) ;
+        return dp[amount] = ans ;
     }
     
-    int coinChange(vector<int>& arr, int amount) {
-          int n = arr.size() ;
-          vector<vector<int>>dp(n,vector<int>(amount+1,0));
-          for(int i=0 ; i<=amount ; ++i){
-              if(i%arr[0]==0){
-                  dp[0][i] = i/arr[0] ;
-              }else{
-                  dp[0][i] = 1e9 ;
-              }
-          }
-       
-         for(int ind = 1; ind<n; ind++){
-        for(int target = 0; target<=amount; target++){
-            int notTake = 0 + dp[ind-1][target];
-            int take = 1e9;
-            if(arr[ind]<=target)
-                take = 1 + dp[ind][target - arr[ind]];
-                
-             dp[ind][target] = min(notTake, take);
-        }
-    }
+    int coinChange(vector<int>& coins, int amount) {
+            memset(dp,-1,sizeof(dp));
+            int ans = func(amount , coins);
         
-        int ans = dp[n-1][amount] ;
-        if(ans>=1e9) return -1 ;
-        return ans ;
-        
+           return ans == INT_MAX ? -1 : ans ;
     }
 };
