fa4af9cb0ce5e92326e7d163e56edb8b04d307d2
Paras Nagpal <paras.nagpal743@gmail.com>
2022-05-22 21:49:21 +0530

    Time: 3 ms (91.94%), Space: 6.3 MB (66.67%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.length() ;
+        int res = 0 ;
+        for(int i=0 ; i<n ; ++i){
+            int left = i , right = i ;
+            while(left>=0 && right<n && s[left]==s[right]){
+                res+=1;
+                left--;
+                right++;
+            }
+            left = i , right = i+1 ;
+             while(left>=0 && right<n && s[left]==s[right]){
+                res+=1;
+                left--;
+                right++;
+            }
+        }
+        
+        return res ;
+    }
+};
