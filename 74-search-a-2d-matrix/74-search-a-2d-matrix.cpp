1dbb5f98030f89fc83c1674afe77b5cf6ea0ddc0
Paras Nagpal <paras.nagpal743@gmail.com>
2022-05-23 03:00:09 +0530

    Time: 19 ms (6.56%), Space: 9.3 MB (95.65%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    bool func(vector<int> &arr , int target){
-         int left = 0 , right = arr.size()-1 ;
-         while(left<=right){
-             int mid = left + (right-left)/2 ;
-             if(arr[mid]==target) return true ;
-             if(arr[mid]<target) left = mid + 1 ;
-             else right = mid -1 ;
-         }
-        return false ;
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-          int n = matrix.size() ;
-          for(int i=0 ; i<n ; ++i){
-              if(func(matrix[i],target)) return true ;
-          }
-        return false ;
+         int n = matrix.size() , m = matrix[0].size() ;
+         if(target < matrix[0][0] || target > matrix[n-1][m-1])  return 0;
+         int start = 0 , end = n-1 ;
+         int col = m-1 , indx ;
+         while(start<=end){
+             int mid = start + (end-start)/2 ;
+             if(matrix[mid][col]==target) return true ;
+             else if(matrix[mid][col]>target) end = mid-1 ;
+             else start = mid + 1 ;
+         }
+        
+        if(start==n) start = n-1 ;
+        if(matrix[start][0]>target) start--;
+        int x = start ;
+        auto it = lower_bound(matrix[x].begin(),matrix[x].end(),target) ;
+        if(it!=matrix[x].end() && *it==target) return true ;
+        else return false ;
+        
     }
 };
