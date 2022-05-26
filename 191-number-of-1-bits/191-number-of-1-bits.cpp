c8488a846bca7e8b55a28b8f9c5aa6ddb88ae563
Paras Nagpal <paras.nagpal743@gmail.com>
2022-05-26 12:13:29 +0530

    Time: 4 ms (36.04%), Space: 5.8 MB (79.17%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+             int cnt = 0 ;
+        for(int i=31 ; i>=0 ; i--){
+            if(n & (1 << i)){
+                cnt++;
+            }
+        }
+        return cnt ;
+    }
+};
