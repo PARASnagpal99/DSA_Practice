5cfe473611dd297490626374a76acd0453003a74
Paras Nagpal <paras.nagpal743@gmail.com>
2022-05-28 10:11:08 +0530

    Time: 679 ms (5.01%), Space: 87.3 MB (5.53%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,32 +1,32 @@
 class Solution {
-public:
-    int dfs(int i , int j , vector<vector<int>>& grid , vector<vector<int>>& dp){
-          int n = grid.size() , m = grid[0].size() ;
-          if(dp[i][j]){
-              return dp[i][j] ;
-          }
-        vector<pair<int,int>>drctns = {{0,1},{-1,0},{1,0},{0,-1}} ;
-        int res = 1 ;
-        for(int a=0 ; a<4 ; ++a){
-            int x = i + drctns[a].first ;
-            int y = j + drctns[a].second ;
-            if(x<0 or y<0 or x>=n or y>=m) continue ;
-            if(grid[x][y]<=grid[i][j]) continue ;
-            res = max(res,1+dfs(x,y,grid,dp));
-        }
-        return dp[i][j] = res ;
-    }
-    
+public :
+   int dfs(int x , int y , vector<vector<int>>& matrix , vector<vector<int>>& dp){
+       int n = matrix.size() , m = matrix[0].size() ;
+       if(dp[x][y]) return dp[x][y] ;
+       vector<vector<int>>dir = {{0,1},{1,0},{0,-1},{-1,0}};
+       int ans = 1 ;
+       for(int i=0 ; i<4 ; ++i){
+           int X = x + dir[i][0] ;
+           int Y = y + dir[i][1] ;
+           
+           if(X<0 or Y<0 or X>=n or Y>=m ) continue ;
+           if(matrix[X][Y]<=matrix[x][y]) continue ;
+           ans = max(ans,1+dfs(X,Y,matrix,dp));
+       }
+       return dp[x][y] = ans ;
+   }
+   
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int n = matrix.size() , m = matrix[0].size() ;
-        if(n==0) return 0 ;
-        vector<vector<int>>dp(n,vector<int>(m,0));
-        int ans = 0 ;
-        for(int i=0 ; i<n ; ++i){
-            for(int j=0 ; j<m ; ++j){
-                ans = max(ans,dfs(i,j,matrix,dp));
+         int n = matrix.size() , m = matrix[0].size() ;
+         if(n==0) return 0 ;
+         vector<vector<int>>dp(n,vector<int>(m,0));
+            int res = 0 ;
+            for(int i=0 ; i<n ; ++i){
+                for(int j=0 ; j<m ; ++j){
+                    if(!dp[i][j]) res = max(res,dfs(i,j,matrix,dp));
+                }
             }
-        }
-        return ans ;
+            
+        return res ;
     }
 };
