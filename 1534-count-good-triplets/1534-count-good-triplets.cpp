fb0c6c02cfd29b105cdc423e433b0624b8b52d75
Paras Nagpal <paras.nagpal743@gmail.com>
2022-06-03 03:24:30 +0530

    Time: 39 ms (75.45%), Space: 8.1 MB (93.31%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.cpp
+++ 1534-count-good-triplets/1534-count-good-triplets.cpp
@@ -5,8 +5,10 @@ public:
         int cnt = 0 ;
         for(int i=0 ; i<n ; ++i){
             for(int j=i+1 ; j<n ; ++j){
-                for(int k=j+1 ; k<n ; ++k){
-                    if(abs(arr[i]-arr[j])<=a && abs(arr[j]-arr[k])<=b && abs(arr[i]-arr[k])<=c) cnt++;
+                if(abs(arr[i]-arr[j])<=a){
+                    for(int k=j+1 ; k<n ; ++k){
+                    if(abs(arr[j]-arr[k])<=b && abs(arr[i]-arr[k])<=c) cnt++;
+                   }
                 }
             }
         }
