6774cda55b77f287cf4b07beffbb44f7f5283364
Paras Nagpal <paras.nagpal743@gmail.com>
2022-06-03 13:19:09 +0530

    Time: 649 ms (40.27%), Space: 148.1 MB (21.70%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -9,17 +9,28 @@ public:
             }
         }
         
+         for(int i=0 ; i<grid[0].size() ; ++i){
+            for(int j=1 ; j<grid.size() ; ++j){
+                  grid[j][i]+=grid[j-1][i] ;
+            }
+        }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
         int sum = 0 ;
-        for(int i=row1 ; i<=row2 ; ++i){
-            sum+=grid[i][col2] ;
-            if(col1>=1){
-                sum-=grid[i][col1-1] ;
-            }
+        sum+=grid[row2][col2] ;
+        if(col1>=1){
+            sum-=grid[row2][col1-1] ;
         }
-     
+         
+        if(row1>=1){
+            sum-=grid[row1-1][col2] ;
+        }
+        
+        if(row1>=1 && col1>=1){
+            sum+=grid[row1-1][col1-1] ;
+        }
+        
         return sum ;
     }
 };
