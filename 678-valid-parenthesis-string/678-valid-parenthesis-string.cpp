a0ea65c17fc8c7a2ce65243af57f86cc61c4fd17
Paras Nagpal <paras.nagpal743@gmail.com>
2022-06-03 12:25:41 +0530

    Time: 5 ms (22.79%), Space: 5.9 MB (99.72%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     /*
-    ( * ))
-    
-    
-    
+     (*))
+      
     
     */
+    
     bool checkValidString(string s) {
-            stack<int> stk1 , stk2 ;
-            for(int i=0 ; i<s.length() ; ++i){
-                if(s[i]=='(') stk1.push(i) ;
-                else if(s[i]=='*') stk2.push(i) ;
-                else{
-                    if(!stk1.empty()) stk1.pop() ;
-                    else if(!stk2.empty()) stk2.pop() ;
-                    else return false ;
-                }
-            }
-          while(!stk1.empty() && !stk2.empty()){
-             if(stk1.top()>stk2.top()) return false ;
-              stk1.pop();
-              stk2.pop();
-          }
-         return stk1.empty() ;
+            int val1 =0 , val2 = 0 ;
+         for(auto &it : s){
+             if(it=='(' or it=='*') val1++;
+             else val1--;
+             
+             if(val1<0) return false ;
+         }
+        
+        for(int i=s.length()-1 ; i>=0 ; i--){
+             if(s[i]==')' or s[i]=='*') val2++;
+             else val2--;
+            if(val2<0) return false ;
+        }
+        
+        return true ;
+        
     }
 };
