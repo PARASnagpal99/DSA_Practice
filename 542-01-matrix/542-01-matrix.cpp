2bc2163560fcf12d3bfb2194bc78a0bb2fbe4af2
Paras Nagpal <paras.nagpal743@gmail.com>
2022-06-08 02:40:16 +0530

    Time: 86 ms (80.52%), Space: 27.5 MB (83.28%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        if (rows == 0)
-            return matrix;
-        int cols = matrix[0].size();
-        vector<vector<int>> dist(rows, vector<int> (cols, INT_MAX));
-        queue<pair<int, int>> q;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0) {
-                    dist[i][j] = 0;
-                    q.push({ i, j }); //Put all 0s in the queue.
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size() , m = mat[0].size() ;
+        if(n==0) return mat ;
+        vector<vector<int>>dp(n,vector<int>(m,INT_MAX-2));
+        
+        for(int i=0 ; i<n ; ++i){
+            for(int j=0 ; j<m ; ++j){
+                if(mat[i][j]==0){
+                    dp[i][j] = 0 ;
+                }
+                
+                if(i > 0){
+                    dp[i][j] = min(dp[i][j], dp[i-1][j] + 1 ) ;
+                }
+                
+                if(j > 0){
+                    dp[i][j] = min(dp[i][j] , dp[i][j-1] + 1 ) ;
+                 }   
+            }
+        }
+        
+        for(int i=n-1 ; i>=0 ; --i){
+            for(int j=m-1 ; j>=0 ; --j){
+                
+                if(i < n-1){
+                    dp[i][j] = min(dp[i][j],dp[i+1][j]+1);
+                }
+                
+                if(j < m-1){
+                    dp[i][j] = min(dp[i][j],dp[i][j+1]+1);
                 }
             }
         }
-
-        int dir[4][2] = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-        while (!q.empty()) {
-            pair<int, int> curr = q.front();
-            q.pop();
-            for (int i = 0; i < 4; i++) {
-                int new_r = curr.first + dir[i][0], new_c = curr.second + dir[i][1];
-                if (new_r >= 0 && new_c >= 0 && new_r < rows && new_c < cols) {
-                    if (dist[new_r][new_c] > dist[curr.first][curr.second] + 1) {
-                        dist[new_r][new_c] = dist[curr.first][curr.second] + 1;
-                        q.push({ new_r, new_c });
-                    }
-                }
-            }
-        }
-        return dist;
+        
+        return dp ;
     }
 };
