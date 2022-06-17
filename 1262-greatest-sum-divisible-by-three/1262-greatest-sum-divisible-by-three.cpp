01aeed820afcbbb9a9d13340902a54ab42998914
Paras Nagpal <paras.nagpal743@gmail.com>
2022-06-18 02:29:43 +0530

    Time: 98 ms (44.00%), Space: 38.4 MB (22.13%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    
-    int func(int i , vector<int> &nums , int state , vector<vector<int>> &dp){
-          if(i == nums.size()){
-              if(state == 0){
-                  return 0 ;
-              }
-              return -10000 ;
-          }    
-        if(dp[i][state]!=-1) return dp[i][state] ;
-        int take = nums[i] + func(i+1 , nums , (state+nums[i])%3 , dp) ;
-        int notTake = func(i+1,nums,state, dp );
-        return dp[i][state] = max(take,notTake);
-    }
-    
-    
-    
     int maxSumDivThree(vector<int>& nums) {
         int n = nums.size() ;
-        vector<vector<int>> dp(n+1,vector<int>(3,-1));
-        int ans = func(0,nums,0,dp);
-        return ans ;
+        vector<vector<int>> dp(n,vector<int>(3,0));
+        
+        dp[0][(nums[0])%3] = nums[0] ;
+        
+        for(int i=1 ; i<n ; ++i){
+            int x = dp[i-1][0] + nums[i] ;
+            int y = dp[i-1][1] + nums[i] ;
+            int z = dp[i-1][2] + nums[i] ;
+            
+            dp[i] = dp[i-1] ;
+            
+            dp[i][x%3] = max(x,dp[i][x%3]);
+            dp[i][y%3] = max(y,dp[i][y%3]);
+            dp[i][z%3] = max(z,dp[i][z%3]);
+        }
+        
+        return dp[n-1][0] ;
         
     }
 };
