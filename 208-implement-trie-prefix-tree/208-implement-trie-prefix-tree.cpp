778741067f58abdcc034d55550c91e33e504cd65
Paras Nagpal <paras.nagpal743@gmail.com>
2022-06-18 18:24:23 +0530

    Time: 62 ms (90.88%), Space: 45 MB (49.88%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -1,23 +1,29 @@
 struct Node{
+    // basic structure of trie , it contains a hashmap with lot of references and a flag 
     Node* links[26];
     bool flag = false ;
     
+    // check if the reference trie is present or not 
     bool containsKey(char ch){
         return (links[ch-'a']!=NULL) ;
     }
     
+    // creating reference trie node 
     void put(char ch , Node* node){
         links[ch-'a'] = node ;
     }
     
+    // to get the next node for traversal 
     Node* get(char ch){
         return links[ch-'a'];
     }
     
+    // setting flag to the true at the end of word 
     void setEnd(){
         flag = true ;
     }
     
+    // checking if the word is completed or not 
     bool isEnd(){
         return flag ;
     }
@@ -28,11 +34,12 @@ class Trie {
 private : Node* root ;
 public:
     Trie() {
+        // creating new object 
         root = new Node() ;
     }
     
     void insert(string word) {
-        Node *node = root ;
+        Node *node = root ;   // initializing dummy node pointing to the root initially 
         for(int i=0 ; i<word.size() ; ++i){
             if(!(node->containsKey(word[i]))){
                 node->put(word[i],new Node());
