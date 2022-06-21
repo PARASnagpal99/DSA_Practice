4fe797c3210f85360ce972c00f920118dff897c5
Paras Nagpal <paras.nagpal743@gmail.com>
2022-06-22 04:29:10 +0530

    Time: 949 ms (5.01%), Space: 94 MB (5.01%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -9,27 +9,84 @@ public:
     when size > ladders and bricks < st.top break and return the current index ;
     
     */
-    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        ll n = heights.size() ;
-        
-        priority_queue<ll, vector<ll> ,greater<ll> > pq;
-        
-        for(ll i=0 ; i<n-1 ; ++i){
-            
-            ll diff = heights[i+1] - heights[i] ;
-            if(diff < 0) continue ;
-            if(diff > 0) pq.push(diff) ;
-            
-            if(pq.size() > ladders){
-                auto curr = pq.top() ;
-                if(bricks < curr) return i ;
-                bricks-=curr;
-                pq.pop() ;
-            }
-            
+    
+    
+    
+    
+    
+    
+    bool isValid(vector<int>&heights , int bricks , int ladders , int k){
+           vector<int> diff ;
+           
+        for(int i=1 ; i<=k ; i++){
+            int d = heights[i] - heights[i-1] ;
+            if(d > 0) diff.push_back(d) ;
         }
         
-        return n-1 ;
+        sort(diff.rbegin(),diff.rend()) ;
+            
+        for(int i=ladders ; i<diff.size() ; ++i){
+              if(bricks < diff[i] ) return false ;
+              bricks-=diff[i] ;
+        }
+        
+        return bricks>=0 ;
+        
+    }
+    
+    
+    
+    
+
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+//         ll n = heights.size() ;
+        
+//         priority_queue<ll, vector<ll> ,greater<ll> > pq;
+        
+//         for(ll i=0 ; i<n-1 ; ++i){
+            
+//             ll diff = heights[i+1] - heights[i] ;
+//             if(diff < 0) continue ;
+//             if(diff > 0) pq.push(diff) ;
+            
+//             if(pq.size() > ladders){
+//                 auto curr = pq.top() ;
+//                 if(bricks < curr) return i ;
+//                 bricks-=curr;
+//                 pq.pop() ;
+//             }
+            
+//         }
+        
+//         return n-1 ;
+        
+        
+         // using Binary search on answer 
+         int n = heights.size() ;  
+         int lo = 0 , hi = n-1 ; 
+         int ans = 0 ;
+         while(lo <= hi){
+             int mid = lo + (hi-lo)/2 ;
+             if(isValid(heights,bricks,ladders,mid)){
+                 ans = mid  ;
+                 lo = mid+1 ;
+             }else{
+                 hi = mid-1 ;
+             }
+         }
+        
+        
+        
+        
+        return ans ;
+        
+        
+        
+        
+        
+        
+        
+        
         
         
     }
