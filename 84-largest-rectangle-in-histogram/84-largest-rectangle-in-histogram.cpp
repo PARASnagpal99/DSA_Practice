c25688930f5ada134a380b014caa57ad5c78fc26
Paras Nagpal <paras.nagpal743@gmail.com>
2022-06-23 00:13:31 +0530

    Time: 176 ms (94.74%), Space: 77.1 MB (72.48%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,51 +1,28 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size() ;
-        vector<int> leftSmaller(n);
-        vector<int> rightSmaller(n) ;
+         int ans = 0 ;
+         int n = heights.size() ;
         
-        stack<int> stk ;
+        stack<int> st ;
         
-        
-        for(int i=0 ; i<n ; ++i){
+        for(int i=0 ; i <= n ; ++i){
             
-            while(!stk.empty() && heights[stk.top()] >= heights[i]){
-                stk.pop() ;
+            while(!st.empty() && (i==n || heights[st.top()] > heights[i] )){
+                
+                int height = heights[st.top()];
+                st.pop() ;
+                int width ;
+                if(st.empty()) width = i ; // no left smaller element 
+                else {
+                    width = i - st.top() - 1 ;  // index of right greater element - index of left smaller element - 1 
+                }
+                
+                ans = max(ans , width * height);
+                
             }
-            
-            if(stk.empty()) leftSmaller[i] = 0 ;
-            else {
-                leftSmaller[i] = stk.top() + 1 ;
-             }
-            
-            stk.push(i) ;
+            if(i!=n) st.push(i);
         }
-        
-        
-        while(!stk.empty()) stk.pop() ;
-        
-        
-        for(int i=n-1 ; i>=0 ; i--){
-            
-            while(!stk.empty() && heights[stk.top()] >= heights[i]){
-                stk.pop() ;
-            }
-            
-            if(stk.empty()) rightSmaller[i] = n-1 ;
-            else {
-                rightSmaller[i] = stk.top() - 1 ;
-             }
-            
-            stk.push(i) ;
-            
-        }
-        
-       int ans = 0 ; 
-       
-       for(int i=0 ; i<n ; ++i){
-           ans = max(ans , heights[i] * (rightSmaller[i]-leftSmaller[i] + 1)) ;
-       } 
         
        return ans ; 
     }
