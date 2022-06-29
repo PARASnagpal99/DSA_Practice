38e131c852370e99b0e8e9ec499e5f7f39713dc7
Paras Nagpal <paras.nagpal743@gmail.com>
2022-06-29 11:21:54 +0530

    Time: 338 ms (13.32%), Space: 24.7 MB (9.64%) - LeetHub

---
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin() , people.end() , [&] (const vector<int> a , const vector<int> b){
+            if(a[0]!=b[0]){
+                return a[0] > b[0] ;
+            }else{
+                return a[1] < b[1] ;
+            }
+        });
+        
+        
+        vector<vector<int>>res ;
+        
+        for(auto &it : people){
+            res.insert(res.begin() + it[1] , it) ;
+        }
+        
+        return res ;
+    }
+};
