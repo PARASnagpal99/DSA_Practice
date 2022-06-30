e2c5032aa633ed02db13ed8ae956af918379de1e
Paras Nagpal <paras.nagpal743@gmail.com>
2022-06-30 11:04:37 +0530

    Time: 12 ms (81.15%), Space: 10.9 MB (15.75%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -3,12 +3,21 @@ public:
     int minMoves2(vector<int>& nums) {
         int n = nums.size() ;
         sort(nums.begin(),nums.end());
-        int target = nums[n/2];
         int cnt = 0 ;
-        for(auto &it : nums){
-            cnt += abs(it - target) ;
+        
+        
+        int left = 0 , right = n-1 ;
+        while(left < right){
+            cnt += (nums[right]-nums[left]);
+            left++;
+            right--;
         }
         
+        
+        
         return cnt ;
+        
+        
+        
     }
 };
