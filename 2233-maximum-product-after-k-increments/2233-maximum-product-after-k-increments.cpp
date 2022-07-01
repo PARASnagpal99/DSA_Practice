ffb21ae33293b76314c7a8b499fc585ad7e8e913
Paras <paras.nagpal743@gmail.com>
2022-07-02 00:38:18 +0530

    Time: 1183 ms (5.14%), Space: 99.4 MB (6.55%) - LeetHub

--- 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.cpp
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums, int k) {
-         multiset<int> st(nums.begin(),nums.end());
-         int mod = 1e9 + 7 ;
+         // min heap
+         priority_queue <int, vector<int>, greater<int>> pq ;
+         
+        for(auto &it : nums) pq.push(it) ;    
+        int mod = 1e9 + 7 ;
         
         while(k--){
-            auto curr = *st.begin() ;
-            st.erase(st.begin()) ;
-            st.insert(curr + 1) ;
+            auto curr = pq.top() ;
+            pq.pop();
+            pq.push(curr + 1) ;
+        }
+        
+        vector<int> res ;
+        while(!pq.empty()){
+            auto curr = pq.top() ;
+            res.push_back(curr) ;
+            pq.pop() ;
         }
         
         long long ans = 1 ;
-        for(auto &it : st){
-            ans = ( ( ans % mod) * (it % mod) ) % mod ; 
+        for(auto &it : res){
+            ans = (ans * it) % mod ;
         }
         
+        ans = ans%mod ;
         return (int)ans ;
         
     }
