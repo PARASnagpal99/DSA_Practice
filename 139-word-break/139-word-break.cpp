7e9cf4c818b0cd0c1eef06b3b4a72b0f8c0840a6
Paras <paras.nagpal743@gmail.com>
2022-07-03 13:26:20 +0530

    Time: 23 ms (45.25%), Space: 9.5 MB (65.88%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,40 +1,6 @@
 class Solution {
 public:
-    vector<int> dp = vector<int>(305,-1) ;
-    
-    
-    bool func(int indx , string &s ,   unordered_set<string> &st ){
-        
-        if(indx == s.size()){
-            return true ;
-        }
-        
-        if(dp[indx]!=-1) return dp[indx] ;
-        
-        string temp ;
-        for(int i=indx ; i<s.size() ; ++i){
-             temp.push_back(s[i]) ;
-             if(st.find(temp) != st.end()){
-                 if(func(i+1,s,st)) return true ;
-             }
-             
-        }
-       return dp[indx] = false ;
-    }
-    
-    
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> st ;
-        for(auto &it : wordDict){
-            st.insert(it) ;
-        }
-        
-        bool ans = func(0,s,st);
-        return ans ;
-        
-    }
-};
-/*
+    /*
     
     1. insert words of dict in unordered set for checking in constant time .
     2. make a recurrence relation , 
@@ -42,6 +8,59 @@ public:
          if(func(i+1)) return true 
        }
     3. memoize karo due to overlapping subproblems .
+       
+       memoization => tabulation . save the space 
+        
     
     
     */
+    
+  //  vector<int> dp = vector<int>(305,-1) ;
+    
+//     bool func(int indx , string &s ,   unordered_set<string> &st ){
+        
+//         if(indx == s.size()){
+//             return true ;
+//         }
+        
+//         if(dp[indx]!=-1) return dp[indx] ;
+        
+//         string temp ;
+//         for(int i=indx ; i<s.size() ; ++i){
+//              temp.push_back(s[i]) ;
+//              if(st.find(temp) != st.end()){
+//                  if(func(i+1,s,st)) return true ;
+//              }
+             
+//         }
+//        return dp[indx] = false ;
+//     }
+    
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.length() ;
+        unordered_set<string> st ;
+        
+        for(auto &it : wordDict){
+            st.insert(it) ;
+        }
+        
+        vector<int> dp(n+1,false) ;
+        dp[0] = true ;
+        
+        for(int i=1 ; i<=n ; ++i){
+            for(int prev = i-1 ; prev >=0 ; prev--){
+                if(dp[prev]){
+                    string temp = s.substr(prev,i-prev) ;
+                    if(st.count(temp)){
+                        dp[i] = true ;
+                        break ;
+                    }
+                }
+            }
+        }
+        
+        return dp[n] ;
+        
+    }
+};
