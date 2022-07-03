91962552af305f0bb86cf4a51dbe3968f4666a55
Paras <paras.nagpal743@gmail.com>
2022-07-03 07:33:44 +0530

    Time: 7 ms (39.62%), Space: 7 MB (75.45%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -2,29 +2,27 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size() ;
-        vector<int> down(n,1) ;
-        vector<int> up(n,1) ;
-        
+        int up = 1 , down = 1 ;
         int maxlen = 1 ;
         
-        for(int i=0 ; i<n ; ++i){
-            for(int prev=0 ; prev < i ; ++prev){
-                if(nums[i] > nums[prev]){
-                    up[i] = max(up[i],1 + down[prev]) ;
-                    down[i] = down[prev] ;
-                }
+        
+        
+        for(int i=1 ; i<n ; ++i){
+            if(nums[i] > nums[i-1]){
                 
-                if(nums[i] < nums[prev]){
-                    down[i] = max(down[i],1+up[prev]);
-                    up[i] = up[prev] ;
-                }
+                up = max(up,1+down);
                 
-            } 
-            maxlen = max(down[i],up[i]);
+            }else if(nums[i] < nums[i-1]){
+                
+                down = max(down,1+up);
+                
+            }
+           
+            maxlen = max(up,down);
+            
         }
         
         return maxlen ;
-        
         
         
         
