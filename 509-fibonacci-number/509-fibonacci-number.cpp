2b2989ab1f0bb2196ee793a453fca6abd2a83371
Paras <paras.nagpal743@gmail.com>
2022-07-07 01:46:22 +0530

    Time: 2 ms (64.31%), Space: 5.9 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        vector<int> dp(31,0);
-        dp[0] = 0 ;
-        dp[1] = 1 ;
+        if(n < 2) return n ;
+        int a = 0 ;
+        int b = 1 ;
+        int ans = 0 ;
         for(int i=2 ; i<=n ; ++i){
-            dp[i] = dp[i-2] + dp[i-1] ;
+            ans = a + b ;
+            a = b ;
+            b = ans ;
         }
-        return dp[n];
+        return ans ;
     }
 };
