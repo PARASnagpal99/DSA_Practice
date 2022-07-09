12ff2acf5ae079e7e4042444d6f9f82f2fd5584a
Paras <paras.nagpal743@gmail.com>
2022-07-09 18:33:33 +0530

    Time: 42 ms (26.49%), Space: 5.9 MB (27.01%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -3,11 +3,12 @@ public:
     // 27->9->3->1
     // 15->5->1
     bool isPowerOfThree(int n) {
-        if(n == 1) return true ;
-        if(n == 0) return false;
-        if(n%3) return false ;
-        bool ans = isPowerOfThree(n/3) ;
-        return ans ;
+        
+        while(n % 3 == 0 && n!=0){
+              n = n/3 ;
+        }
+        
+        return n==1 ;
         
     }
 };
