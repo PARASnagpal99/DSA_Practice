c5495487a76aae380ccb487f0f400e17052e9c2b
Paras <paras.nagpal743@gmail.com>
2022-07-10 06:16:15 +0530

    Time: 8 ms (68.42%), Space: 13.6 MB (79.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    int func(int indx , vector<int>&nums , vector<int> &dp ){
-        int n = nums.size() ;
-        if(indx == n) return 0 ;
-        if(indx > n) return 1e7 ;
-        if(dp[indx]!=-1) return dp[indx] ;
-        
-        int cost = 0 ;
-        
-        int step1 = nums[indx] + func(indx+1,nums,dp) ;
-        
-        int step2 = nums[indx] + func(indx+2,nums,dp) ;
-        
-        cost = min(step1,step2) ;
-        
-        return dp[indx] = cost ;
-        
-    }
-    
-    
-    
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size() ;
-        vector<int>dp1(n+1,-1) ;
-        int step0 = func(0,cost,dp1) ;
-        vector<int>dp2(n+1,-1) ;
-        int step1 = func(1,cost,dp2) ;
-        return min(step0,step1) ;
+        vector<int> dp(n,0) ;
+        
+        dp[0] = cost[0] ;
+        dp[1] = cost[1] ;
+        
+        for(int i=2 ; i<n ; ++i){
+            dp[i] = cost[i] + min(dp[i-1],dp[i-2]) ;
+        }
+        
+        int ans = min(dp[n-1],dp[n-2]);
+        return ans ;
+        
     }
 };
