1bd21eca4e199996402d211d86b9651d7b07ef07
Paras <paras.nagpal743@gmail.com>
2022-07-13 01:25:03 +0530

    Time: 37 ms (84.04%), Space: 11.4 MB (26.80%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,34 +1,39 @@
 class Solution {
- bool backtrack(int curr,int &target,int partitionNum,vector<int>&match,int index,vector<bool>&seen){
-        if(partitionNum==3){
-            return true;
-        } else if(curr==target){
-            return backtrack(0,target,partitionNum+1,match,0,seen);
-        } else {
-            for(int i=index;i<match.size();i++){
-                if(!seen[i]){
-                    seen[i]=true;
-                    if(backtrack(curr+match[i],target,partitionNum,match,i+1,seen)){
-                        return true;
-                    }
-                    seen[i]=false;
-                }
-            }
-            return false;
-        }
-    }
 public:
-    bool makesquare(vector<int>& match) {
-        int total=0;
-        for(int i=0;i<match.size();i++){
-            total=total+match[i];
+    int dp[1 << 16][4] ;
+    int func(int mask , vector<int> &nums , int currsum , int target , int count){
+        int n = nums.size() ;
+        
+        if(count == 0) return 1 ;
+        
+        if(dp[mask][count]!=-1) return dp[mask][count] ;
+        
+        if(currsum == target){
+            return dp[mask][count] = func(mask,nums,0,target,count-1);
         }
-        if(total%4!=0){
-            return false;
-        } else {
-            int target=total/4;
-            vector<bool>seen(match.size(),false);
-            return backtrack(0,target,0,match,0,seen);
+        
+        
+        for(int i=0 ; i<n ; ++i){
+            if((mask & (1 << i)) or currsum + nums[i] > target) continue ;
+            
+            if(func(mask | (1 << i) , nums , currsum + nums[i] , target , count)){
+                return dp[mask][count] = 1 ;
+            }
+            
         }
+        
+        return dp[mask][count] = 0 ;
+    }
+
+        
+    
+    bool makesquare(vector<int>& matchsticks) {
+        int n = matchsticks.size() ;
+        int sum = accumulate(matchsticks.begin(),matchsticks.end(),0);
+        if(sum%4 or n < 4) return false ;
+        memset(dp,-1,sizeof(dp));
+        int target = sum/4 ;
+        int ans = func(0,matchsticks,0 , target,3);
+        return ans ;
     }
 };
