27afc00f98a30402e90fb4b963d776595300512c
Paras <paras.nagpal743@gmail.com>
2022-07-14 10:50:44 +0530

    Time: 160 ms (75.82%), Space: 28.7 MB (77.08%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
+    int dp[2001][2001] ;
+    
+    bool func(int indx , int prev_jump , vector<int> &stones){
+        int n = stones.size() ;
+        if(indx == n-1) return true ;
+        if(indx >= n) return false ;
+        
+        if(dp[indx][prev_jump]!=-1) return dp[indx][prev_jump] ;
+        
+        bool ans = false ;
+        
+        for(int j=-1 ; j<=1 ; ++j){
+            int jump = prev_jump + j ;
+            if(jump > 0){
+                int i = lower_bound(stones.begin(),stones.end(),stones[indx]+jump) - stones.begin() ; 
+                
+                if(indx >= n or stones[i] != stones[indx] + jump) continue ;
+                
+                ans = ans || func(i,jump,stones) ;
+            }
+        }
+        
+        return dp[indx][prev_jump] = ans ;
+    }
+    
+    
     bool canCross(vector<int>& stones) {
-       int n = stones.size() ;
-        
-       unordered_map<int,unordered_set<int>>mp ;
-        
-       mp[stones[0] + 1] = {1} ; 
-       
-       for(int i=1 ; i<n ; ++i){
-           
-           int pos = stones[i] ;
-           
-           for(auto it : mp[pos]){
-               mp[pos+it].insert(it) ;
-               mp[pos+it+1].insert(it+1) ;
-               mp[pos+it-1].insert(it-1) ;
-           }
-           
-       } 
-       
-       return (mp[stones[n-1]].size() > 0) ; 
-        
-        
+            memset(dp,-1,sizeof(dp));
+            bool ans = func(0,0,stones) ;
+            return ans ;
     }
 };
