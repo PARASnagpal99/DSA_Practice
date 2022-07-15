d039e2d1fae1fd043f65630159732f9dbbc7e85d
Paras <paras.nagpal743@gmail.com>
2022-07-15 08:30:03 +0530

    Time: 67 ms (6.67%), Space: 26.9 MB (13.01%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     void dfs(int i , int j , vector<vector<int>>&grid , int &count){
         int n = grid.size() , m = grid[0].size() ;
         if(i < 0 or j < 0 or i>=n or j>=m or grid[i][j] == 0) return ; 
@@ -14,24 +15,39 @@ public:
     
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int n = grid.size() , m = grid[0].size() ;
+        int m = grid.size() , n = grid[0].size() ;
         
-        int mx_area = 0 ;
         
-        int cnt ;
+        vector<pair<int,int>> dirs = {{0,-1},{0,1},{-1,0},{1,0}};
         
-        for(int i=0 ; i<n ; ++i){
-            for(int j=0 ; j<m ; ++j){
-                if(grid[i][j]){
-                    cnt = 0 ;
-                    dfs(i,j,grid,cnt) ;
-                    mx_area = max(mx_area,cnt) ;
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    stack<pair<int,int>> s;
+                    s.push({i,j});
+                    int count=0;
+                    while(!s.empty()){
+                        int x=s.top().first,y=s.top().second;
+                        s.pop();
+                        
+                        if(grid[x][y]==0) continue;
+                            
+                        count++;
+                        grid[x][y]=0;
+                        
+                        for(int k=0;k<4;k++){
+                            int x1=x+dirs[k].first,y1=y+dirs[k].second;
+                            if(x1>=0 && x1<m && y1>=0 && y1<n && grid[x1][y1]==1){
+                                s.push({x1,y1});
+                            }
+                        }
+                    }
+                    ans=max(ans,count);
                 }
             }
         }
-        
-        
-        return mx_area ;
+        return ans;
         
     }
 };
