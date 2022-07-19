b81222403947b528290a6056b58f12d64c088157
Paras <paras.nagpal743@gmail.com>
2022-07-19 11:38:50 +0530

    Time: 921 ms (44.24%), Space: 209.4 MB (40.01%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,27 +1,64 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
-using namespace __gnu_pbds;
-
-#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>
-
-
 class Solution {
-public:    
-    vector<int> countSmaller(vector<int>& nums) {
-        int n = nums.size() ;
-        vector<int> ans(n) ;
-        ordered_set st ;
-        int indx = n-1 ;
-        
-        while(indx >= 0){
-            st.insert(nums[indx]);
-            int cnt = st.order_of_key(nums[indx]) ;
-            ans[indx] = cnt ;
-            indx--;
+public:
+void merge(vector<int> &count ,vector<pair<int,int>>&v , int left , int mid , int right ){
+    int i = left ;
+    int j = mid + 1 ;
+    int k = 0 ;
+    
+    vector<pair<int,int>>temp(right-left+1);
+    
+    while(i<=mid && j<=right){
+        if(v[i].first <= v[j].first){
+            temp[k++] = v[j++];
+        }else{
+            count[v[i].second] += (right-j+1) ;
+            temp[k++] = v[i++] ;
         }
-        
-        
-        return ans ;
+    }
+    
+    
+    while(i <= mid){
+        temp[k++] = v[i++] ;
+    }
+    
+    while(j <= right){
+        temp[k++] = v[j++] ;
+    }
+    
+    for(int i=left ; i<=right ; ++i){
+        v[i] = temp[i-left] ;
+    }
+}
+
+
+
+
+
+void mergeSort(vector<int> &count , vector<pair<int,int>>& v , int left , int right){
+    if(left >= right) return ;
+    
+    int mid = left + (right - left)/2 ;
+    
+    mergeSort(count,v,left,mid);
+    mergeSort(count,v,mid+1,right);
+    merge(count,v,left,mid,right);
+    
+}
+
+    vector<int> countSmaller(vector<int>& arr) {
+        int n = arr.size();
+        vector<pair<int,int>>nums(n) ;
+	    
+	    for(int i=0 ; i<n ; ++i){
+	        nums[i] = make_pair(arr[i],i) ;
+	    }
+	    
+	    vector<int> res(n) ;
+	    
+	    mergeSort(res,nums,0,n-1);
+	    
+	    
+	    return res ;
+	    
     }
 };
