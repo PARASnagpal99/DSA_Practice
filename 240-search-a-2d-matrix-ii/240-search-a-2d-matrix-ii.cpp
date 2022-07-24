ade0124f9d1998800149ae16b85ab0bc78dda3e2
Paras <paras.nagpal743@gmail.com>
2022-07-24 08:55:18 +0530

    Time: 467 ms (10.48%), Space: 14.8 MB (88.55%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -2,17 +2,12 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size() , m = matrix[0].size() ;
-        int i = 0 , j = m-1 ;
-        while(i < n && j >= 0){
-            if(matrix[i][j]==target) return true ;
-            
-            if(matrix[i][j] > target){
-                j--;
-            }else if(matrix[i][j] < target){
-                i++;
-            }
+        for(int i=0 ; i<n ; ++i){
+            int indx = lower_bound(matrix[i].begin(),matrix[i].end(),target) - matrix[i].begin() ;
+            if((indx <= m-1 ) && matrix[i][indx] == target) return true ;
         }
         
         return false ;
+        
     }
 };
