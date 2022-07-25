7ab1830e4c7e65f616fd88a1c999a4966f34861c
Paras <paras.nagpal743@gmail.com>
2022-07-25 13:01:12 +0530

    Time: 10 ms (73.39%), Space: 13.7 MB (17.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,11 +1,49 @@
 class Solution {
 public:
+    int lower_bound(vector<int>&nums , int left , int right , int target){
+        
+        while(left < right){
+            int mid = left + (right-left)/2 ;
+            
+            if(nums[mid] >= target){
+                right = mid;
+            }else{
+                left = mid + 1 ;
+            }
+        }
+        
+        
+        if(left < nums.size() && nums[left] < target) left++;
+        
+        return left ;
+    }
+    
+    
+    int upper_bound(vector<int>&nums , int left , int right , int target){
+        
+               while(left < right){
+                   int mid = left + (right-left)/2 ;
+                   
+                   if(nums[mid] <= target){
+                       left = mid + 1 ;
+                   }else{
+                       right = mid;
+                   }
+               }
+        if(left < nums.size()  && nums[left] <= target){
+            left++;
+        }
+        return left ;
+    }
+    
+    
+    
     vector<int> searchRange(vector<int>& nums, int target) {
         
         int n = nums.size() ;
         
-        int first = lower_bound(nums.begin(),nums.end(),target) - nums.begin() ;
-        int second = upper_bound(nums.begin(),nums.end(),target) - nums.begin() ;
+        int first = lower_bound(nums,0,n-1,target) ;
+        int second = upper_bound(nums,0,n-1,target) ;
         second-- ;
         
         if(first < n && nums[first] == target){
