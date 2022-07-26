bf72bdac4f9b10922e9dbc9306d6d992fd141005
Paras <paras.nagpal743@gmail.com>
2022-07-26 09:58:47 +0530

    Time: 24 ms (54.17%), Space: 15.1 MB (15.85%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,20 +9,42 @@
  */
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    bool func(TreeNode* root , vector<int> &path , int target){
+        if(root == NULL) return false ;
         
-        if(!root or root == p or root == q){
-            return root ;
+        path.push_back(root->val) ;
+        
+        if(root->val == target) return true ;
+        
+        bool l = func(root->left,path,target) ;
+        bool r = func(root->right,path,target) ;
+        bool check = l | r ;
+        if(check) return true ;
+        
+        path.pop_back() ;
+        return false ;
+    }
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL or root == p or root == q) return root ;
+        vector<int> path1 ;
+        vector<int> path2 ;
+        
+        func(root,path1,p->val);
+        func(root,path2,q->val);
+        
+        int i = 0 , j = 0 ;
+        
+        while(i < path1.size() && j < path2.size() && path1[i] == path2[j]){
+                i++ ;
+                j++ ;
         }
         
-        TreeNode* l = lowestCommonAncestor(root->left,p,q);
-        TreeNode* r = lowestCommonAncestor(root->right,p,q);
+        i--;
         
-        if(l == NULL && r == NULL) return NULL ;
-        else if(l!=NULL && r!=NULL) return root ;
-        else if(l == NULL) return r ;
-        else
-            return l ;
+        TreeNode* ans = new TreeNode(path1[i]) ;
+        
+        return ans ;
         
     }
 };
